a6185f44f3eeb0a8da45ff50ffad29d7ded42445
lnkz <lnkz80@gmail.com>
2022-06-10 18:11:47 +0300

    CLASSES - need to reMade

--- js/script.js
+++ js/script.js
@@ -128,25 +128,80 @@ document.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }
     });
-    
+
     //close modal by pressing "Escape"
     document.addEventListener("keydown", (e) => {
         if (e.code === "Escape" && modalWindow.classList.contains('show')) {
             closeModal();
         }
     });
-    
+
     //show modal after 3s 
-    const modalIntervalId = setTimeout(openModal, 3000);
-    
+    // const modalIntervalId = setTimeout(openModal, 3000);
+
     //show modal at the end of page
     const modalScrollOpen = () => {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             openModal();
             window.removeEventListener('scroll', modalScrollOpen);
         }
     };
 
     window.addEventListener('scroll', modalScrollOpen);
+
+    //CARDS "MENU"
+
+    const DB = [{
+            header: 'Меню "Фитнес"',
+            text: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            price: 229,
+            img: "img/tabs/vegy.jpg"
+        },
+        {
+            header: 'Меню “Премиум”',
+            text: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            price: 550,
+            img: "img/tabs/elite.jpg"
+        },
+        {
+            header: 'Меню "Постное"',
+            text: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            price: 430,
+            img: "img/tabs/post.jpg"
+        }
+    ];
+
+
+
+    class Card {
+        constructor(db, parentDiv) {
+            this.db = db;
+            this.parentDiv = parentDiv;
+            this.html = "";
+            this.parentDiv = document.querySelector(this.parentDiv);
+        }
+
+        makeHTML() {
+            this.db.forEach(item => {
+                this.html += `
+                    <div class="menu__item">
+                        <img src=${item.img} alt="vegy">
+                        <h3 class="menu__item-subtitle">${item.header}</h3>
+                        <div class="menu__item-descr">${item.text}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${item.price}</span> грн/день</div>
+                        </div>
+                    </div>
+                `;
+            });
+            this.parentDiv.innerHTML = this.html;
+        }
+    }
+
+    const div = new Card(DB, ".menu .container");
+
+    div.makeHTML();
 
 });
